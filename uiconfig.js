1ae226723e6851f637c969aab776ac6e7d24d34e
Vivian Chan <viviancpy@gmail.com>
2016-08-08 00:20:17 +1200

    use https

--- uiconfig.js
+++ uiconfig.js
@@ -1,5 +1,5 @@
 ﻿var config = {
-    Spots_Api: 'http://azureeightydayswebapp.azurewebsites.net/api/v1.0/spots/',
+    Spots_Api: 'https://azureeightydayswebapp.azurewebsites.net/api/v1.0/spots/',
     Places_Api: 'http://localhost:30002/api/v1.0/places/',
     Photos_Api: 'http://localhost:30002/api/v1.0/photos/'
 }
